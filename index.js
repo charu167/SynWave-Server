c176b167a58b03fdd63d0c11340fab987bf7fa9d
Charudatta Potdar <charupot08@gmail.com>
2024-01-16 16:30:21 -0700

    listening on process.env.PORT

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("<h1>HIIIIIIII</h1>");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
 
