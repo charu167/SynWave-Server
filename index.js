614e03ef6b2818e1730dcda30fbc545a131caae1
Charudatta Potdar <charupot08@gmail.com>
2024-01-17 14:59:28 -0700

    temp reverted

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ require("dotenv").config();
 const app = express();
 
 //node middleware
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-// //Importing routes
-// const authRouter = require("./routes/auth.routes");
-// const playlistRouter = require("./routes/playlist.routes");
-// const userRouter = require("./routes/user.routes");
+//Importing routes
+const authRouter = require("./routes/auth.routes");
+const playlistRouter = require("./routes/playlist.routes");
+const userRouter = require("./routes/user.routes");
 
-// //Routing
-// app.use("/auth", authRouter);
-// app.use("/playlists", playlistRouter);
-// app.use("/user", userRouter);
+//Routing
+app.use("/auth", authRouter);
+app.use("/playlists", playlistRouter);
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("<h1>HIIIIIIII</h1>");
@@ -27,11 +27,11 @@ app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${process.env.PORT || 3000}`);
 });
 
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then((e) => {
-//     console.log("Connected to mongoose");
-//   })
-//   .catch((error) => console.log(error));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((e) => {
+    console.log("Connected to mongoose");
+  })
+  .catch((error) => console.log(error));
 
 module.exports = app
